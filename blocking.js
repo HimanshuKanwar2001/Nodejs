ebe9fae21158cae96bb61f26b121d15dcae2139f
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-09-13 19:09:00 +0530

    first commit

---
+++ blocking.js
@@ -0,0 +1,16 @@
+// for(let i=0; i<10000000000000;i++){
+//     //I am doing nothiung just consuming time.
+// }
+// console.log("Task to pe performed after loop");
+
+
+// console.log(" I have to wait for this long loop to finish");
+
+
+
+setTimeout(()=>{
+    console.log('task to be performed after 8 sec');
+},8000);
+
+
+console.log("I do not have to wati for the long loop to finish ");
