0c992f2fac74a8d1371479cd7aeb3fc2af536b70
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-09-15 01:06:14 +0530

    how express works

--- lecture-4/express-server.js
+++ lecture-4/express-server.js
@@ -1,15 +1,21 @@
-const express=require('express');
+const express = require("express");
 
 //Create a server.
-const server=express();
-
+const server = express();
 
 //Handle for default request
-server.get("/",(req,res)=>{
-    res.send("Welcome to Express Server");
+server.get("/", (req, res, next) => {
+  console.log("First middleware hit");
+  next();
+});
+server.get("/", (req, res) => {
+  res.send("Welcome to Express Server");
 });
 
 //Listen to specified port.
-server.listen(3100,()=>{
-    console.log("server is listening on 3100");
+server.listen(3200, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("server is listening on 3200");
 });
