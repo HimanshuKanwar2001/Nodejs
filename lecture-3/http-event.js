a7ff2d05a0a2a8d7312217b8659d0f8917de8d4f
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-09-14 10:25:49 +0530

    testing API using Postman

--- lecture-3/http-event.js
+++ lecture-3/http-event.js
@@ -1,18 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    if(req.method== "POST"){
-        //excepting data from client
-        let body="";
-        req.on("data",(chunk)=>{
-                body+=chunk.toString();
-        });
-        req.on("end",()=>{
-            console.log(body);
-            res.end("Data is received");
-        })
-    }
-  res.end("Welcome to node js");
+  if (req.method == "POST") {
+    console.log(req.body);
+    //excepting data from client
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      console.log(body);
+      res.end("Data is received");
+    });
+  } else {
+    console.log("Functions ends here");
+    res.end("Welcome to node js");
+  }
 });
 
 server.listen(3100);
