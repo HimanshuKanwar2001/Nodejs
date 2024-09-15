ff5e99d343c4f1c92501e6a597690e930444e842
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-09-15 08:27:49 +0530

    creating layout and seperating body from  header and footer and dynamically showing in between the header and footer

--- lecture-5/inventory-app/index.js
+++ lecture-5/inventory-app/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import ProductController from './src/controllers/product.controller.js';
 import path from 'path';
+import expressEjsLayouts from 'express-ejs-layouts';
 
 const server=express();
 
 //setup view engine settings
 server.set("view engine","ejs");
 server.set("views",path.join(path.resolve(),"src","views"))
+server.use(expressEjsLayouts);
+
 
 //create an instance of ProductController
 const productController=new ProductController();
