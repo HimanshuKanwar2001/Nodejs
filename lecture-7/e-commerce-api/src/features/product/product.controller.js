fccafd0345220d4b7981138fdb2b6e6945707c5d
unknown <himanshukanwar2001@gmail.com>
2024-09-22 15:29:55 +0530

    Product Controller

--- lecture-7/e-commerce-api/src/features/product/product.controller.js
+++ lecture-7/e-commerce-api/src/features/product/product.controller.js
@@ -1,6 +1,9 @@
+import ProductModel from "./product.model.js";
+
 export default class ProductController{
         getAllProducts(req,res){
-
+                const products=ProductModel.getAll();
+                res.status(200).send(products);
         }
 
         addProduct(req,res){
