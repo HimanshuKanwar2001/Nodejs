6549298d5dc419e3a143c867c12891d2674f0e05
HimanshuKanwar2001 <himanshukanwar2001@gmail.com>
2024-09-29 22:39:17 +0530

    added api documentation using swagger 2.0

--- lecture-7-8-9/e-commerce-api/server.js
+++ lecture-7-8-9/e-commerce-api/server.js
@@ -1,24 +1,29 @@
 //1.Import Express
 import express from "express";
+import swagger from "swagger-ui-express";
+
 import ProductRouter from "./src/features/product/product.routes.js";
 import UserRouter from "./src/features/user/user.routes.js";
 import bodyParser from "body-parser";
-import path from 'path';
+import path from "path";
 import jwtAuth from "./src/middleware/jwt.middleware.js";
 import cartRouter from "./src/features/cart/cartitems.routes.js";
+import apiDoc from "./swagger.json" assert { type: "json" };
 
 //2.Create Server
 const app = express();
 
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(bodyParser.json()); //   or     app.use(express.json());
 
-app.use(express.static(path.join(path.resolve(),"public")));
-app.use(bodyParser.json()); //   or     app.use(express.json()); 
-
+//Bearer <token>
 //for all requests related to product,redirect to product routes
 //localhost:3200/api/products
-app.use("/api/products",jwtAuth, ProductRouter);
-app.use("/api/cartItems",jwtAuth,cartRouter);
-app.use("/api/users",UserRouter);
+app.use("/api-docs", swagger.serve, swagger.setup(apiDoc));
+
+app.use("/api/products", jwtAuth, ProductRouter);
+app.use("/api/cartItems", jwtAuth, cartRouter);
+app.use("/api/users", UserRouter);
 
 //3.Default requrest handler
 app.get("/", (req, res) => {
